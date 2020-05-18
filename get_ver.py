224276500c54add66901300b8b7784a1ce8f7bfa
Nicholas Shpetner <nicholas.shpetner@uconn.edu>
2020-05-18 13:17:22 -0400

    Inital commit

---
+++ get_ver.py
@@ -0,0 +1,67 @@
+import requests
+import json
+import sys
+def get_most_recent_version_forecast():
+    '''Gets most recent release for the BGPextrap'''
+    req = requests.get('https://api.github.com/repos/nickup9/BGPExtrapolator/tags')
+    req_json = req.json()
+    tags = {}
+    for i in req_json:
+        tags[i['name']] = i['commit']['sha']
+    max_ver = '0.0.0'
+    max_tag = ''
+    for tag in tags.keys():
+        if tag[0:5] != 'bgpe-':
+            continue
+        ver = tag[6:]
+        max_ver = ver_cmp(max_ver, ver)
+        if max_ver == ver:
+            max_tag = tag
+    return(max_tag, tags[max_tag][0:7])
+
+def get_most_recent_version_rov():
+    '''Gets the most recent release of rov++'''
+    req = requests.get('https://api.github.com/repos/nickup9/BGPExtrapolator/tags')
+    req_json = req.json()
+    tags = {}
+    for i in req_json:
+        tags[i['name']] = i['commit']['sha']
+    max_ver = '0.0.0'
+    max_tag = ''
+    for tag in tags.keys():
+        if tag[0:4] != 'rov-':
+            continue
+        ver = tag[5:]
+        max_ver = ver_cmp(max_ver, ver)
+        if ver == max_ver:
+            max_tag = tag
+    return(max_tag, tags[max_tag][0:7])
+def convert_ver(ver):
+    '''Converts semantic version to just the numbers.'''
+    count = ver.count('v')
+    split = ver.split('v', count)
+    return split[len(split)-1]
+    # It just works.
+##################
+#Helper Functions#
+##################
+def ver_cmp(ver_cur, ver_new):
+    '''compares two versions'''
+    for i in range(3):
+        if int(ver_new.split('.')[i]) > int(ver_cur.split('.')[i]):
+            return ver_new
+        if int(ver_new.split('.')[i]) < int(ver_cur.split('.')[i]):
+            return ver_cur
+# for the bash script/installer/packager
+argv = sys.argv
+if argv[1] == 'forecast-sha':
+    print(get_most_recent_version_forecast()[1])
+if argv[1] == 'rov-sha':
+    print(get_most_recent_version_rov()[1])
+# Holy inefficency, batman
+if argv[1] == 'forecast-tag':
+    print(get_most_recent_version_forecast()[0])
+if argv[1] == 'rov-tag':
+    print(get_most_recent_version_rov()[0])
+if argv[1] == 'convert':
+    print(convert_ver(argv[2]))
---
+++ packager.sh
@@ -0,0 +1,240 @@
+#!/bin/bash
+
+#TODO: PR for makefile
+#      sed makefile before packaging	
+# Making sure that jq and python3 are installed
+python3_test=$(command -v python3)
+jq_test=$(command -v jq)
+if [ -z "$python3_test" ]
+then
+	echo "Aborting: python3 not installed"
+	exit 1
+fi
+if [ -z "$jq_test" ]
+then
+	echo "Aborting: jq not installed"
+	exit 1
+fi
+
+
+
+# Getting forecast release commit shas
+fore_rel_sha="$(python3 get_ver.py forecast-sha)"
+fore_rel_ver="$(python3 get_ver.py forecast-tag)"
+echo "Forcast Release: " $fore_rel_sha
+echo "From: "$fore_rel_ver
+
+# Getting forecast nightly commit shas
+url_branch='https://api.github.com/repos/c-morris/BGPExtrapolator/branches/master'
+fore_night_sha=$(curl -s $url_branch | jq -r '.commit.sha')
+fore_night_cut=$(echo $fore_night_sha | cut -c1-7)
+echo "Forecast Nightly: " $fore_night_cut
+
+# Getting rov release commit shas
+rov_rel_sha="$(python3 get_ver.py rov-sha)"
+rov_rel_ver="$(python3 get_ver.py rov-tag)"
+echo "rov Release: " $rov_rel_sha
+echo "From: " $rov_rel_ver
+
+# Getting rov nightly commit urls
+url_branch='https://api.github.com/repos/nickup9/BGPExtrapolator/branches/rovpp2'
+rov_night_sha=$(curl -s $url_branch | jq -r '.commit.sha')
+rov_night_cut=$(echo $rov_night_sha | cut -c1-7)
+echo "rov Nightly: " $rov_night_cut
+
+# Get the debian metadata file:
+url_branch='https://api.github.com/repos/c-morris/BGPExtrapolator-debian-package-metadata/branches/master'
+debian_sha=$(curl -s $url_branch | jq -r '.commit.sha')
+debian_cut=$(echo $debian_sha | cut -c1-7)
+echo "debian metadata: " $debian_cut
+# Download it:
+dwnload_url=('https://codeload.github.com/c-morris/BGPExtrapolator-debian-package-metadata/tar.gz/'$debian_cut)
+output_tar=("debian.tar.gz")
+output_dir=('debian')
+echo "Getting debian from: "$dwnload_url
+curl -l $dwnload_url --output $output_tar
+mkdir $output_dir && tar -xf $output_tar -C $output_dir --strip-components 2
+# Clean up the debian tarball, we don't need it
+rm $output_tar
+# Save the debian's location for later use
+deb=$(pwd)/debian
+
+# Now to download sources and build. We will start with bgp-extrap-release first
+dwnload_url=('https://codeload.github.com/nickup9/BGPExtrapolator/tar.gz/'$fore_rel_sha)
+# Get the version
+ver="$(python3 get_ver.py convert $fore_rel_ver)"
+type=('stable')
+# Make formatted name for unpacked and packed source
+output_tar=('bgp-extrapolator-'$type'_'$ver'.orig.tar.gz')
+output_dir=('bgp-extrapolator-'$type'_'$ver)
+echo "Getting bgp-extrapolator-stable from: "$dwnload_url
+# Make and go into a new dir to save on clutter
+mkdir $output_dir && cd $output_dir
+# Download the tarball
+curl -l $dwnload_url --output $output_tar
+
+# Make a dir for the tarball to extract into and extract
+mkdir $output_dir && tar -xf $output_tar -C $output_dir --strip-components 1
+# Remove the tarball. We're gonna make a new one later.
+rm -r $output_tar
+
+# copy the debian into the new dir
+cp -r $deb $output_dir
+# cd into directory for building and more work
+cd $output_dir
+
+# convert all references to bgp-extrapolator in deb files to bgp-extrapolator-$type
+# Fix the makefile
+sed -i "s/bgp-extrapolator/bgp-extrapolator-$type/g" Makefile
+# Go into the debian folder
+cd debian
+# Change all folder names
+for file in bgp-extrapolator* ; do mv $file ${file//bgp-extrapolator/bgp-extrapolator-$type} ; done
+# Change content within folders to reflect the type
+for file in * ; do sed -i "s/bgp-extrapolator/bgp-extrapolator-$type/g" $file ;done
+# Take two steps out to get out of the debian and source
+cd .. && cd ..
+
+# Rebuild the tarball
+tar -czf $output_tar $output_dir
+# Go back into the output dir for building
+cd $output_dir
+# Build it
+debuild -us -uc
+# Get back out into the top level of the packager
+cd .. && cd ..
+
+# Now for the forecast nightly
+dwnload_url=('https://codeload.github.com/nickup9/BGPExtrapolator/tar.gz/'$fore_night_cut)
+type=('unstable')
+output_tar=('bgp-extrapolator-'$type'.orig.tar.gz')
+output_dir=('bgp-extrapolator-'$type)
+echo "Getting bgp-extrapolator-unstable from: "$dwnload_url
+
+# Make and go into a new dir to save on clutter
+mkdir $output_dir && cd $output_dir
+# Download it
+curl -l $dwnload_url --output $output_tar
+# Unpack it into a new dir
+mkdir $output_dir && tar -xf $output_tar -C $output_dir --strip-components 1
+# Remove the tar to rebuild later
+rm -r $output_tar
+# Copy debian into this new dir
+cp -r $deb $output_dir
+# Go into the dir
+cd $output_dir
+
+# convert all references to bgp-extrapolator in deb files to bgp-extrapolator-$type
+# Fix the makefile
+sed -i "s/bgp-extrapolator/bgp-extrapolator-$type/g" Makefile
+# Go into the debian
+cd debian
+# change file names to reflect type
+for file in bgp-extrapolator* ; do mv $file ${file//bgp-extrapolator/bgp-extrapolator-$type} ; done
+# change content in files to reflect type
+for file in * ; do sed -i "s/bgp-extrapolator/bgp-extrapolator-$type/g" $file ;done
+# Get the version of this nightly release from the changelog
+ver=$(grep -o -m 1  '.\..\..' changelog)
+
+# go back to the dir with tarball and unpacked dir
+cd .. && cd ..
+# rebuild our tarball
+tar -czf $output_dir'_'$ver'.orig.tar.gz' $output_dir
+
+# Edit dir and tarball names with the version of the nightly
+mv $output_dir $output_dir'_'$ver
+
+# Go into the dir to build
+cd $output_dir'_'$ver
+
+# Build it
+debuild -us -uc
+
+# Go back out for the next package
+cd .. && cd ..
+
+# Next: rov release
+dwnload_url=('https://codeload.github.com/nickup9/BGPExtrapolator/tar.gz/'$rov_rel_sha)
+ver="$(python3 get_ver.py convert $rov_rel_ver)"
+type=('stable')
+output_tar=('rov-'$type'_'$ver'.orig.tar.gz')
+output_dir=('rov-'$type'_'$ver)
+echo "Getting rov-stable from: "$dwnload_url
+# Make and go into a new dir to save on clutter
+mkdir $output_dir && cd $output_dir
+curl -l $dwnload_url --output $output_tar
+
+# Make a dir for the tarball to extract into and extract
+mkdir $output_dir && tar -xf $output_tar -C $output_dir --strip-components 1
+# Remove the tarball to rebuild later
+rm -r $output_tar
+# copy the debian into the new dir
+cp -r $deb $output_dir
+
+# cd into directory for building
+cd $output_dir
+
+# convert all references to bgp-extrapolator in deb files to rov-$type
+# Makefile changes
+sed -i "s/bgp-extrapolator/rov-$type/g" Makefile
+# deb changes
+cd debian
+for file in bgp-extrapolator* ; do mv $file ${file//bgp-extrapolator/rov-$type} ; done
+for file in * ; do sed -i "s/bgp-extrapolator/rov-$type/g" $file ;done
+# Leave deb and source
+cd .. && cd .. 
+
+# Rebuild tarball
+tar -czf $output_tar $output_dir
+# Go back into output dir for building
+cd $output_dir
+# Build it
+debuild -us -uc
+# Get back out into the top level of the packager
+cd .. && cd ..
+
+# Now for the rov nightly
+dwnload_url=('https://codeload.github.com/nickup9/BGPExtrapolator/tar.gz/'$rov_night_cut)
+type=('unstable')
+output_tar=('rov-'$type'.orig.tar.gz')
+output_dir=('rov-'$type)
+echo "Getting rov-unstable from: "$dwnload_url
+
+# Make and go into a new dir to save on clutter
+mkdir $output_dir && cd $output_dir
+# Download it
+curl -l $dwnload_url --output $output_tar
+# Unpack it into a new dir
+mkdir $output_dir && tar -xf $output_tar -C $output_dir --strip-components 1
+# Remove the tar to rebuild later
+rm -r $output_tar
+# Copy debian into this new dir
+cp -r $deb $output_dir
+# Go into the dir
+cd $output_dir
+
+# convert all references to bgp-extrapolator in deb files to bgp-extrapolator-$type
+# Fix the makefile
+sed -i "s/bgp-extrapolator/rov-$type/g" Makefile
+# Go into the debian
+cd debian
+# change file names to reflect type
+for file in bgp-extrapolator* ; do mv $file ${file//bgp-extrapolator/rov-$type} ; done
+# change content in files to reflect type
+for file in * ; do sed -i "s/bgp-extrapolator/rov-$type/g" $file ;done
+# Get the version of this nightly release from the changelog
+ver=$(grep -o -m 1  '.\..\..' changelog)
+
+# go back to the dir with tarball and unpacked dir
+cd .. && cd ..
+# rebuild our tarball
+tar -czf $output_dir'_'$ver'.orig.tar.gz' $output_dir
+
+# Edit dir and tarball names with the version of the nightly
+mv $output_dir $output_dir'_'$ver
+
+# Go into the dir to build
+cd $output_dir'_'$ver
+
+# Build it
+debuild -us -uc
